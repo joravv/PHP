ab9b6498a64814e787ffccde37c3a6743a007080
joravv <joravulaa@gmail.com>
2026-01-15 20:31:54 +0100

    mod3

---
+++ mod2/index.php
@@ -0,0 +1,30 @@
+<?php
+$emri= "Digiyalschool";
+echo $emri="i go to ";
+
+$x=120;
+$y=50;
+
+echo $x+$y. "<hr>";
+echo $x-$y."<hr>";
+echo $x/$y."<hr>";
+echo $x*$y."<hr>";
+
+$emri = "buna";
+$mbiemri= "berisha";
+$username= "bunaberisha";
+
+echo $emri.$mbiemri;
+
+$thestring = "Buna Berisha";
+echo strlen($thestring);
+
+$string5 = " fcb plays today";
+echo str_word_count($string5);
+
+$string6= " rma is the best";
+echo str_replace("best", "worst", $string6);
+
+$string7= "FCb";
+echo strrev($string7);
+?>
---
+++ mod3/index.php
@@ -0,0 +1,8 @@
+<?php
+$nr = 4;
+if($nr ==0){
+    echo "numri eshte 0"
+}else if($nr>0){
+    echo "numri eshte me i madh se 0"
+}
+?>
