2d7b69e4e007b78078d87a6dd2d6ef5384669de9
joravv <joravulaa@gmail.com>
2026-01-20 20:50:31 +0100

    3

--- mod3/index.php
+++ mod3/index.php
@@ -1,8 +1,74 @@
 <?php
 $nr = 4;
 if($nr ==0){
-    echo "numri eshte 0"
+    echo "numri eshte 0";
 }else if($nr>0){
-    echo "numri eshte me i madh se 0"
+    echo "numri eshte me i madh se 0";
 }
+
+
+
+$day = "wednasday";
+switch($day){
+    case "Monday" ;
+    echo "yay i hate school";
+break;
+case "Monday" ;
+    echo "yay i hate school";
+break;
+
+case "Tuesday" ;
+    echo "idk bro ";
+break;
+
+case "Wednesday" ;
+    echo "skip school";
+break;
+
+case "Thursday" ;
+    echo "skip school again";
+break;
+
+case "Friday" ;
+    echo "wait one more day";
+break;
+
+case "saturday" ;
+    echo "out with friends";
+break;
+case "Sunday" ;
+    echo "school tomorrow";
+break;
+
+deffault:
+echo "error";
+break;
+
+}
+
+$numri = 0;
+
+while($numri <=5){
+echo "the number is $numri <hr>";
+$numri++;
+
+}
+
+
+$numrii=0;
+do{
+
+    echo "the number is $numrii";
+$numrii++;
+}while($numrii <=5)
+
+$x =0{
+    for($x=0; $x<=10; $x++);
+echo "the number is $x";
+
+}
+
+
+
+
 ?>
