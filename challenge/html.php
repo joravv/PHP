7d1946b18dd333f885786dda2420b217016b6654
joravv <joravulaa@gmail.com>
2026-01-29 20:16:38 +0100

    challenge3

--- challenge/html.php
+++ challenge/html.php
@@ -16,5 +16,11 @@ echo $pi*$r^2.;
 $string7= "FCb";
 echo strrev($string7);
 
+$age=14;
+if($age==18){
+    echo "personi mund te votoj";
+}else if($age<18){
+    echo "personi nuk mund te votoj";
+}
 
 ?>
